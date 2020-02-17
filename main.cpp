80056871099875ed9a2efe8124e97614ab5464be
Kristian Brünn <kristian.brunn@epfl.ch>
2020-02-17 14:19:45 +0100

    simply neb_msg_t type to send dynamic sized messages

--- main.cpp
+++ main.cpp
@@ -12,6 +12,32 @@ static struct hrd_qp_attr_t **bcst_qps;
 static struct hrd_qp_attr_t **repl_qps;
 static struct hrd_ctrl_blk_t *cb;
 
+struct neb_msg_t {
+  uint64_t id;
+  void *data;
+  size_t len;
+
+  size_t marshall(uint8_t *buf) {
+    size_t data_size = len * sizeof(uint8_t);
+    size_t msg_size = 2 * sizeof(id) + data_size;
+
+    buf[0 * sizeof(uint64_t)] = id;
+    buf[1 * sizeof(uint64_t)] = data_size;
+
+    memcpy(&buf[2 * sizeof(uint64_t)], data, msg_size);
+
+    return msg_size;
+  };
+
+  void unmarshall(uint8_t *buf) {
+    id = (uint64_t)buf[0 * sizeof(uint64_t)];
+    len = (uint64_t)buf[1 * sizeof(uint64_t)];
+    data = (void *)&buf[2 * sizeof(uint64_t)];
+  }
+
+  size_t size() { return 2 * sizeof(id) + len; }
+};
+
 int run_poller() {
   printf("main: poller thread running\n");
 
@@ -21,17 +47,26 @@ int run_poller() {
         continue;
       }
 
-      const int offset = 128;
+      const size_t offset = 1024;
 
-      auto *val = reinterpret_cast<volatile uint64_t *>(cb->conn_buf[i * 2]);
+      auto *bcast_buf =
+          reinterpret_cast<volatile uint8_t *>(cb->conn_buf[i * 2]);
 
-      if (*val != 0) {
-        printf("main: bcast from %d = %zu\n", i, *val);
+      neb_msg_t msg;
+
+      msg.unmarshall((uint8_t *)bcast_buf);
+
+      // TODO(Kristian): add more checks like matching next id etc.
+      if (msg.id != 0) {
+        printf("main: bcast from %d = %s\n", i, (char *)msg.data);
+
         auto *repl_buf =
-            reinterpret_cast<volatile uint64_t *>(cb->conn_buf[i * 2 + 1]);
-        repl_buf[i] = *val;
+            reinterpret_cast<volatile uint8_t *>(cb->conn_buf[i * 2 + 1]);
 
-        printf("main: set replay_buf[%d] = %lu\n", i, repl_buf[i]);
+        memcpy((void *)&repl_buf[i * msg.size()], (void *)bcast_buf,
+               msg.size());
+
+        printf("main: copying to replay-buffer\n");
 
         // read replay slots for origin i
         for (int j = 0; j < num_nodes; j++) {
@@ -41,13 +76,14 @@ int run_poller() {
 
           struct ibv_sge sg;
           struct ibv_send_wr wr;
-          struct ibv_wc wc;
+          // struct ibv_wc wc;
           struct ibv_send_wr *bad_wr = nullptr;
 
           memset(&sg, 0, sizeof(sg));
-          sg.addr = reinterpret_cast<uint64_t>(cb->conn_buf[i * 2 + 1]) +
-                    (offset + i * num_nodes + j) * sizeof(uint64_t);
-          sg.length = sizeof(uint64_t);
+          sg.addr =
+              reinterpret_cast<uint64_t>(cb->conn_buf[i * 2 + 1]) +
+              (offset + (i * num_nodes + j) * msg.size()) * sizeof(uint8_t);
+          sg.length = msg.size();
           sg.lkey = cb->conn_buf_mr[i * 2 + 1]->lkey;
 
           memset(&wr, 0, sizeof(wr));
@@ -55,8 +91,8 @@ int run_poller() {
           wr.sg_list = &sg;
           wr.num_sge = 1;
           wr.opcode = IBV_WR_RDMA_READ;
-          wr.send_flags = IBV_SEND_SIGNALED;
-          wr.wr.rdma.remote_addr = repl_qps[j]->buf_addr + i * sizeof(uint64_t);
+          // wr.send_flags = IBV_SEND_SIGNALED;
+          wr.wr.rdma.remote_addr = repl_qps[j]->buf_addr + i * msg.size();
           wr.wr.rdma.rkey = repl_qps[j]->rkey;
 
           printf("main: Posting replay read for %d at %d\n", i, j);
@@ -66,14 +102,17 @@ int run_poller() {
             return -1;
           }
 
-          hrd_poll_cq(cb->conn_cq[j * 2 + 1], 1, &wc);
+          // hrd_poll_cq(cb->conn_cq[j * 2 + 1], 1, &wc);
 
           if (bad_wr != nullptr) {
             printf("bad_wr is set!\n");
           }
 
-          printf("main: replay entry in loop for %d at %d = %zu\n", i, j,
-                 repl_buf[(offset + i * num_nodes + j)]);
+          neb_msg_t r_msg;
+
+          r_msg.unmarshall((uint8_t *)&repl_buf[(offset + (i * num_nodes + j) * msg.size())]);
+          printf("main: replay entry for %d at %d = %s\n", i, j,
+                 (char *)r_msg.data);
         }
       }
     }
@@ -88,13 +127,21 @@ int run_poller() {
 int main(int argc, char *argv[]) {
   rt_assert(argc > 1);
 
-  num_nodes = atoi(argv[1]);
-  local_gid = atoi(argv[2]);
+  local_gid = atoi(argv[1]);
+
+  // otherwise default is 4
+  if (argc == 3) {
+    num_nodes = atoi(argv[2]);
+  }
 
   bcst_qps = (hrd_qp_attr_t **)malloc(num_nodes * sizeof(hrd_qp_attr_t));
   repl_qps = (hrd_qp_attr_t **)malloc(num_nodes * sizeof(hrd_qp_attr_t));
 
-  hrd_ibv_devinfo();
+  // hrd_ibv_devinfo();
+
+  // ---------------------------------------------------------------------------
+  // ------------------------------ CONTROL PATH -------------------------------
+  // ---------------------------------------------------------------------------
 
   printf("main: Begin control path\n");
 
@@ -117,13 +164,13 @@ int main(int argc, char *argv[]) {
 
     char srv_name[kHrdQPNameSize];
     sprintf(srv_name, "broadcast-%d-%zu", i, local_gid);
+    hrd_publish_conn_qp(cb, i * 2, srv_name);
     printf("main: Node %zu published broadcast slot for node %d\n", local_gid,
            i);
-    hrd_publish_conn_qp(cb, i * 2, srv_name);
 
     sprintf(srv_name, "replay-%zu-%d", local_gid, i);
-    printf("main: Node %zu published replay slot for node %d\n", local_gid, i);
     hrd_publish_conn_qp(cb, i * 2 + 1, srv_name);
+    printf("main: Node %zu published replay slot for node %d\n", local_gid, i);
   }
 
   // Connect to "boradcast-self-x" QP and "replay-x-self" QP published by x
@@ -184,10 +231,19 @@ int main(int argc, char *argv[]) {
     hrd_wait_till_ready(clt_name);
   }
 
+  // ---------------------------------------------------------------------------
+  // -------------------------------- DATA PATH --------------------------------
+  // ---------------------------------------------------------------------------
   printf("main: Broadcast and replay connections established\n");
   printf("main: Begin data path\n");
 
   std::thread poller_thread = std::thread(run_poller);
+
+  char text[kHrdQPNameSize];
+  sprintf(text, "Hello From %zu", local_gid);
+
+  printf("%s", text);
+  struct neb_msg_t msg = {.id = 1, .data = (void *)&text, .len = sizeof(text)};
 
   // Broadcast: write to every "broadcast-self-x" qp
   for (int i = 0; i < num_nodes; i++) {
@@ -195,20 +251,22 @@ int main(int argc, char *argv[]) {
       continue;
     }
 
-    const int offset = 128;
+    const size_t offset = 256;
 
     struct ibv_sge sg;
     struct ibv_send_wr wr;
     // struct ibv_wc wc;
     struct ibv_send_wr *bad_wr = nullptr;
 
-    auto *buf = reinterpret_cast<volatile uint64_t *>(cb->conn_buf[i * 2]);
-    buf[offset] = local_gid + 100;
+    auto *buf =
+        reinterpret_cast<volatile uint8_t *>(cb->conn_buf[i * 2]) + offset;
+
+    size_t msg_size = msg.marshall((uint8_t *)buf);
 
     memset(&sg, 0, sizeof(sg));
-    sg.length = sizeof(uint64_t);
+    sg.length = msg_size;
     sg.addr = reinterpret_cast<uint64_t>(cb->conn_buf[i * 2]) +
-              offset * sizeof(uint64_t);
+              offset * sizeof(uint8_t);
     sg.lkey = cb->conn_buf_mr[i * 2]->lkey;
 
     memset(&wr, 0, sizeof(wr));
