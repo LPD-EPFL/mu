e30cf439768b2782ef7728a89205fae97667ade4
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-05-21 13:53:16 +0200

    Add variable payload size and store measurements to file

--- crash-consensus/demo/main.cpp
+++ crash-consensus/demo/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <fstream>
 #include <stdexcept>
 #include <vector>
 #include <chrono>
@@ -8,8 +9,24 @@
 #include <dory/crash-consensus.hpp>
 #include "timers.h"
 
+void mkrndstr_ipa(int length, uint8_t *randomString) {
+    static uint8_t charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    if (length) {
+        if (randomString) {
+            int l = (int) (sizeof(charset) - 1);
+            for (int n = 0; n < length; n++) {
+                int key = rand() % l;
+                randomString[n] = charset[key];
+            }
+
+            randomString[length] = '\0';
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
+  if (argc < 3) {
     throw std::runtime_error("Provide the id of the process as argument");
   }
 
@@ -26,15 +43,15 @@ int main(int argc, char* argv[]) {
     case '3':
       id = 3;
       break;
-    case '4':
-      id = 4;
-      break;
-    case '5':
-      id = 5;
-      break;
     default:
       throw std::runtime_error("Invalid id");
   }
+
+  int payload_size = atoi(argv[2]);
+  std::vector<uint8_t> payload_buffer(payload_size + 2);
+  uint8_t *payload = &payload_buffer[0];
+
+  std::cout << "USING PAYLOAD SIZE = " << payload_size << std::endl;
 
   // Build the list of remote ids
   std::vector<int> remote_ids;
@@ -48,44 +65,47 @@ int main(int argc, char* argv[]) {
 
   uint64_t commit_id = 0;
   dory::Consensus consensus(id, remote_ids);
-  consensus.commitHandler([&commit_id]([[maybe_unused]] bool leader, uint8_t* buf, size_t len) {
-    if (len != sizeof(uint64_t)) {
-      std::cout << "The committed value must be a uint64_t for this test"
-                << std::endl;
-    } else {
-      uint64_t val = *reinterpret_cast<uint64_t*>(buf);
-      uint64_t proposer_id = val >> 60;
-      val &= 0xfffffffffffffffUL;
+  consensus.commitHandler([&commit_id]([[maybe_unused]] bool leader, [[maybe_unused]]uint8_t* buf, [[maybe_unused]]size_t len) {
+    // if (len != sizeof(uint64_t)) {
+    //   std::cout << "The committed value must be a uint64_t for this test"
+    //             << std::endl;
+    // } else {
+    //   uint64_t val = *reinterpret_cast<uint64_t*>(buf);
+    //   uint64_t proposer_id = val >> 60;
+    //   val &= 0xfffffffffffffffUL;
 
-      (void)proposer_id;
+    //   (void)proposer_id;
 
-      // std::cout << "CID: " << commit_id
-      //           << ", Proposer: " << proposer_id
-      //           << ", Val: " << val << "\n";
-      commit_id += 1;
-    }
+    //   // std::cout << "CID: " << commit_id
+    //   //           << ", Proposer: " << proposer_id
+    //   //           << ", Val: " << val << "\n";
+    //   commit_id += 1;
+    // }
   });
 
   // Wait enough time for the consensus to become ready
   std::cout << "Wait before starting to propose" << std::endl;
   // Give some slack to id 1 to become leader
   std::this_thread::sleep_for(std::chrono::seconds(5 + 3 * id));
-  std::cout << "Started proposing" << std::endl;
+  std::cout << "Started" << std::endl;
 
-  if (id == 1 || id == 2) {
+  if (id == 1) {
     TIMESTAMP_INIT;
 
-    const uint64_t times = 1024 * 1024;
-    std::vector<TIMESTAMP_T> timestamps(times);
+    const uint64_t times = 1000000;
+    std::vector<TIMESTAMP_T> timestamps_start(times);
+    std::vector<TIMESTAMP_T> timestamps_end(times);
+
+
 
     for (uint64_t i = 0; i < times; i++) {
-      GET_TIMESTAMP(timestamps[i]);
+      mkrndstr_ipa(payload_size, payload);
+
+      GET_TIMESTAMP(timestamps_start[i]);
       // Encode process doing the proposal
-      uint64_t encoded_i = i | (uint64_t(id) << 60);
       dory::ProposeError err;
       // std::cout << "Proposing " << i << std::endl;
-      if ((err = consensus.propose(reinterpret_cast<uint8_t*>(&encoded_i),
-                                    sizeof(encoded_i))) != dory::ProposeError::NoError) {
+      if ((err = consensus.propose(payload, payload_size)) != dory::ProposeError::NoError) {
         // std::cout << "Proposal failed at index " << i << std::endl;
         i -= 1;
         switch (err) {
@@ -119,30 +139,31 @@ int main(int argc, char* argv[]) {
       } else {
         // std::cout << "Proposed " << i << std::endl;
 
-        if (i % 5 == 0) {
-          std::cout << "Passed " << i << std::endl;
-        }
+        // if (i % 5 == 0) {
+        //   std::cout << "Passed " << i << std::endl;
+        // }
       }
 
+      GET_TIMESTAMP(timestamps_end[i]);
+
       // std::cout << std::endl;
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+      // std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     std::cout << "Finished proposing, computing rtt for proposals..."
               << std::endl;
-    // Adjacent difference
-    std::vector<uint64_t> diffs;
-    auto it = timestamps.begin();
-    auto prev = *it++;
-    for (; it < timestamps.end(); it++) {
-      diffs.push_back(ELAPSED_NSEC(prev, (*it)));
-      prev = *it;
+
+    std::ofstream dump;
+    dump.open ("dump.txt");
+
+    for (size_t i = 0; i < timestamps_start.size(); i++) {
+      dump << ELAPSED_NSEC(timestamps_start[i], timestamps_end[i]) << "\n";
     }
 
-    for (auto n : diffs) {
-      std::cout << n << std::endl;
-    }
+    dump.close();
   }
+
+  std::cout << "Done" << std::endl;
 
   while (true) {
     std::this_thread::sleep_for(std::chrono::seconds(60));
