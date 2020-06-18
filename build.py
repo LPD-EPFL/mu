9b1dd4bb6df6beb3d6207acc584787eeb337036d
Kristian Brünn <kristian.brunn@epfl.ch>
2020-06-18 18:28:56 +0200

    allow to specify build type in build.py (#28)

--- build.py
+++ build.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
 
 import subprocess
 import os
 
-make_list = subprocess.check_output(['make', 'list'])
-make_list = make_list.decode('utf-8').split()
-make_list = [x.strip() for x in filter(lambda x: not x.endswith('mangled'), make_list)]
+make_list = subprocess.check_output(["make", "list"])
+make_list = make_list.decode("utf-8").split()
+make_list = [x.strip() for x in filter(lambda x: not x.endswith("mangled"), make_list)]
+
 
 def removeFromList(lst, value):
     try:
@@ -19,39 +21,56 @@ def removeFromList(lst, value):
     except ValueError:
         return []
 
+
 # Bring all, clean, distclean in front and sort the rest
-removed = removeFromList(make_list, 'all') + removeFromList(make_list,
-        'clean') + removeFromList(make_list, 'distclean')
+removed = (
+    removeFromList(make_list, "all")
+    + removeFromList(make_list, "clean")
+    + removeFromList(make_list, "distclean")
+)
 make_list.sort()
 make_list = removed + make_list
 
 
 import argparse
+
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 
 parser.add_argument(
-    '-c',
-    '--compiler',
-    choices=['gcc', 'clang'],
-    default='gcc',
-    nargs='?',
-    dest='compiler',
-    help='Type of the compiler (default: gcc)',
-    )
+    "-b",
+    "--build-type",
+    choices=["debug", "release", "relwithdebinfo", "minsizerel"],
+    default="release",
+    nargs="?",
+    dest="build_type",
+    help="Type of build (default: release)",
+)
+
+parser.add_argument(
+    "-c",
+    "--compiler",
+    choices=["gcc", "clang"],
+    default="gcc",
+    nargs="?",
+    dest="compiler",
+    help="Type of the compiler (default: gcc)",
+)
 
 descr = """What targets to build (default: all)
 Possible targets:
 {}
-""".format('\n'.join(make_list))
+""".format(
+    "\n".join(make_list)
+)
 
-parser.add_argument('TARGET', default='all', nargs='*',
-                    choices=make_list, metavar='TARGET', help=descr)
+parser.add_argument(
+    "TARGET", default="all", nargs="*", choices=make_list, metavar="TARGET", help=descr
+)
 
 results = parser.parse_args()
 
-compiler = 'gcc'
-if results.compiler is not None:
-    compiler = results.compiler
+compiler = results.compiler
+build_type = results.build_type
 
 if isinstance(results.TARGET, str):
     targets = [results.TARGET]
@@ -59,21 +78,28 @@ else:
     targets = list(results.TARGET)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-if compiler == 'gcc':
-    CONAN_PROFILE = os.path.join(current_dir, 'conan/profiles',
-                                 'gcc-release.profile')
-    CC='gcc'
-    CXX='g++'
 
-elif compiler == 'clang':
-    CONAN_PROFILE = os.path.join(current_dir, 'conan/profiles',
-                                 'clang-release.profile')
-    CC='clang'
-    CXX='clang++'
+if compiler == "gcc":
+    CONAN_PROFILE = os.path.join(
+        current_dir, "conan/profiles", "gcc-{}.profile".format(build_type)
+    )
+    CC = "gcc"
+    CXX = "g++"
+
+elif compiler == "clang":
+    CONAN_PROFILE = os.path.join(
+        current_dir, "conan/profiles", "clang-{}.profile".format(build_type)
+    )
+    CC = "clang"
+    CXX = "clang++"
 
 # The compilers need to be explicity set, as conan won't interfere with the
 # build system. Therefore, make sure the CC/CXX flags match the conan profile.
-ret = subprocess.call('make CONAN_PROFILE={} CC={} CXX={} {}'.format(CONAN_PROFILE,
-               CC, CXX, ' '.join(targets)), shell=True)
+ret = subprocess.call(
+    "make CONAN_PROFILE={} CC={} CXX={} {}".format(
+        CONAN_PROFILE, CC, CXX, " ".join(targets)
+    ),
+    shell=True,
+)
 
 exit(ret)
---
+++ conan/profiles/gcc-debug.profile
@@ -0,0 +1,14 @@
+[settings]
+os=Linux
+os_build=Linux
+arch=x86_64
+arch_build=x86_64
+compiler=gcc
+compiler.version=7
+compiler.libcxx=libstdc++11
+compiler.cppstd=17
+build_type=Debug
+[options]
+[build_requires]
+[env]
+
